e796882b1a8c99af155008472aa20a5fc57eecbb
Anirban Mukherjee <raji08xd@gmail.com>
2025-01-07 15:22:33 +0530

    Update logger.py
    Changed from skimage.draw import circle to from skimage.draw import ellipse as circle

--- logger.py
+++ logger.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 import imageio
 
 import os
-from skimage.draw import circle
+from skimage.draw import ellipse as circle
 
 import matplotlib.pyplot as plt
 import collections
